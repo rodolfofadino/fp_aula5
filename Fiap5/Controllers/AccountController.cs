978a38bfb58db347a7db7aba7a9bb6e475168c71
RodolfoFadino <rodolfo.fadino@gmail.com>
2017-09-24 23:50:38 -0300

    Add project files.

---
+++ Fiap5/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Fiap5.Models;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+
+namespace Fiap5.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(User user)
+        {
+            if (user.UserName == "rodolfo" && user.Password == "123Mudar")
+            {
+                var claims = new List<Claim>();
+                claims.Add(new Claim(ClaimTypes.Name, user.UserName));
+                var id = new ClaimsIdentity(claims,"password");
+                var principal = new ClaimsPrincipal(id);
+
+                await HttpContext.SignInAsync("app", principal);
+            }
+            return View();
+        }
+
+        public IActionResult Forbidden()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync("app");
+
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Persistent { get; set; }
+    }
+}
---
+++ Fiap5/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Fiap5.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Fiap5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult About()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                ViewData["Message"] = $"Your login: {User.Identity.Name}";
+            }
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
